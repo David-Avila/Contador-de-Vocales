0808dd5169f37787e467fabbc01bdc5d7a626403
David-Avila <aviladurandavid613@gmail.com>
2024-06-17 18:16:03 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 function contarVariables(){
     const inputString = document.getElementById("input").value;
-    const vocals = "aeiouAEIOU";
+    const vowels = "aeiouAEIOUáéíóúÁÉÍÓÚÀÈÌÒÙÄËÏÖÜÅĖÎÔÛ";
     let amount = 0;
 
     if (inputString){
         for (let i = 0; i < inputString.length; i++){
-            if (vocals.includes(inputString[i])){
+            if (vowels.includes(inputString[i])){
                 amount++;
             }
         }
